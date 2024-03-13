117a1beecd3563b100b5a5c22c88b6833db9e80c
Ahmad Syarifuddin <syarifuddinahmads.me@gmail.com>
2024-03-13 10:31:24 +0700

    register auth route

--- internal/http/http.go
+++ internal/http/http.go
@@ -2,12 +2,14 @@ package http
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/syarifuddinahmads/dhswe-marketplace-project-openidea/internal/app/auth"
 	"github.com/syarifuddinahmads/dhswe-marketplace-project-openidea/internal/app/product"
 	"github.com/syarifuddinahmads/dhswe-marketplace-project-openidea/internal/app/user"
 	"github.com/syarifuddinahmads/dhswe-marketplace-project-openidea/internal/factory"
 )
 
 func NewHttp(e *echo.Echo, f *factory.Factory) {
+	auth.NewHandler(f).Route(e.Group("/auth"))
 	product.NewHandler(f).Route(e.Group("/products"))
 	user.NewHandler(f).Route(e.Group("/users"))
 }
